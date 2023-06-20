91c4f72c3d187d71164c060696276d5d57e33e9b
Abdollah Ashrafi <abdollahashrafi1385@gmail.com>
2023-06-21 02:53:11 +0330

    improved styling of qaris page and fixed the pagination selector

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,32 +6,33 @@ function getPerPageSelectOptions(rowsCount) {
         }
     }
     perPageSelectOptions.push(["نمایش همه"]);
+    console.log(perPageSelectOptions)
     return perPageSelectOptions;
 }
 
 function getDataTableTemplate(options) {
     return `<div class='${options.classes.top} fixed-table-toolbar'>
     ${options.paging && options.perPageSelect ?
-        `<div class='${options.classes.dropdown} bs-bars float-left'>
+            `<div class='${options.classes.dropdown} bs-bars float-left'>
             <label>
                 <select class='${options.classes.selector}'></select>
             </label>
         </div>` :
-        ""
-    }
+            ""
+        }
     ${options.searchable ?
-        `<div class='${options.classes.search} float-right search btn-group'>
+            `<div class='${options.classes.search} float-right search btn-group'>
             <input class='${options.classes.input} form-control search-input' placeholder='جست و جو' type='search' title='جست و جو'>
         </div>` :
-        ""
-    }
+            ""
+        }
 </div>
 <div class='${options.classes.container}'${options.scrollY.length ? ` style='height: ${options.scrollY}; overflow-Y: auto;'` : ""}></div>
 <div class='${options.classes.bottom}'>
     ${options.paging ?
-        `<div class='${options.classes.info}'></div>` :
-        ""
-    }
+            `<div class='${options.classes.info}'></div>` :
+            ""
+        }
     <nav class='${options.classes.pagination}'></nav>
 </div>`;
 }

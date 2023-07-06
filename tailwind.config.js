e2bff20f92454148772b7f47d00c6bf767a92f3a
Abdollah Ashrafi <abdollahashrafi1385@gmail.com>
2023-07-06 11:31:48 +0330

    adding color pallete in talwind.config & main-src.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ['./public/*.html'],
   theme: {
     extend: {
+      colors: {
+        'light-primary': '#4ECDC4',
+        'primary': '#006C67',
+        'dark-primary': '#003844',
+        secondary: '#8075FF',
+        light: '#F6FFFF'
+      }
     },
   },
   plugins: [],

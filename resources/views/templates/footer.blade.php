b263f9f56cebe160365b2c446b4a2ec597541ffe
Edris Ranjbar <34417212+edrisranjbar@users.noreply.github.com>
2023-08-18 04:23:15 +0330

    fix #20

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -21,7 +21,7 @@
 <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('assets/js/jquery.easing.min.js') }}"></script>
 <script src="{{ asset('assets/js/sb-admin-2.js') }}"></script>
-{{-- <script src="{{ asset('assets/js/script.js') }}"></script> --}}
+<script src="{{ asset('assets/js/data-tables.js') }}" defer></script>
 </body>
 
 </html>
